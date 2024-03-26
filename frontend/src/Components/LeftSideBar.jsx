c95855230d8566449d369f5ed2b437514001349b
Sbabu201 <152692430+Sbabu201@users.noreply.github.com>
2024-03-26 12:56:53 +0530

    updated the basics

---
+++ frontend/src/Components/LeftSideBar.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import logo from "../assets/babu.jpeg"
+import fb from "../assets/fb.png"
+import insta from "../assets/insta3.png"
+import git from "../assets/git2.png"
+import linledin from "../assets/linkedin2.png"
+import { FcPhone, FcFeedback, FcCalendar, FcDownload, FcList } from "react-icons/fc";
+import { FaLocationDot } from "react-icons/fa6";
+import RightSideBar from './RightSideBar'
+import AboutUs from '../Pages/AboutUs'
+const LeftSideBar = () => {
+    const [togle, setTogle] = useState(false);
+    return (
+        <div className='w-full h-screen '>
+            {/* <div className=' justify-end items-end w-full pr-5 fixed md:hidden flex z-30 '>
+                <button onClick={() => setTogle(state => !state)} ><FcList size={28} /></button>
+                <AboutUs />
+            </div> */}
+            <div className='w-[90%] h-[95%] flex relative flex-col md:mt-0  items-center '>
+                <div className='w-[60%] h-[25%] Z-30 absolute flex items-center justify-center mt-10'>
+                    <img src={logo} alt="logo" className='w-[80%] h-[80%] object-cover  rounded-md' />
+                </div>
+                <div className='w-[90%] flex flex-col items-center h-[75%] mt-40  bg-white  rounded-lg shadow-2xl'>
+                    <div className='mt-20 items-center text-center  w-[90%] h-10'>
+                        <span className='uppercase font-bold text-xs py-2 px-4 rounded-md '>
+                            Soumya sundar mohapatra
+                        </span>
+                    </div>
+                    <div className=' items-center text-center  w-[90%] h-10'>
+                        <span className='uppercase  font-semibold hover:bg-blue-500 hover:text-white cursor-pointer text-xs py-2 px-4 rounded-md  bg-gray-200'>
+                            SoftWare developer
+                        </span>
+                    </div>
+                    <div className='flex justify-evenly mt-4 w-[70%] h-10 '>
+                        <img src={fb} className='object-cover w-6 h-6' alt="" />
+                        <img src={insta} className='object-cover w-10 h-6' alt="" />
+                        <img src={git} className='object-cover w-6 h-6' alt="" />
+                        <img src={linledin} className='object-cover w-6 h-6' alt="" />
+
+
+                    </div>
+                    <div className='bg-gray-300 rounded-md shadow-md w-[90%] h-[40%] flex flex-col justify-evenly'>
+                        <div className='flex gap-4 items-center border-gray-200 border-b-2 w-full h-1/4'><FcPhone size={26} />
+                            <div className='flex flex-col'>
+                                <p className='text-xs'>phone</p>
+                                <p className='font-bold'>6372745765</p>
+                            </div>
+                        </div>
+                        <div className='flex gap-4 items-center border-gray-200 border-b-2 w-full h-1/4'><FcFeedback size={26} />
+                            <div className='flex flex-col'>
+                                <p className='text-xs'>E-mail</p>
+                                <a href='mailto:soumyasundarmohapatra@gmail.com' target='blank' className=' text-wrap text-xs font-bold'>soumyasundarmohapatra@gmail.com</a>
+                            </div>
+                        </div>
+                        <div className='flex gap-4 items-center border-gray-200 border-b-2 w-full h-1/4'><FaLocationDot className='text-red-500' size={26} />
+                            <div className='flex flex-col'>
+                                <p className='text-xs'>Location</p>
+                                <p className='font-bold'>Bhubaneswar , India</p>
+                            </div>
+                        </div>
+                        <div className='flex gap-4 items-center  w-full h-1/4'><FcCalendar size={26} />
+                            <div className='flex flex-col'>
+                                <p className='text-xs'>Birthday</p>
+                                <p className='font-bold'>02-nov , 2001</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='mt-10 w-full h-12 flex justify-center items-center'>
+
+                        <button className='bg-blue-500 hover:bg-blue-900 text-white font-bold rounded-md h-full w-1/2 flex px-2 justify-start gap-2 items-center text-xs'>  <FcDownload size={22} />DOWNLOAD-CV</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LeftSideBar
---
+++ frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { aboutSection } from '../assets/constants'
+const HomePage = () => {
+    return (
+        <div className='w-full h-screen flex justify-center overflow-hidden items-center custom-scrollbar '>
+            <div className=' mt-20 h-[90%] w-full mr-4   overflow-y-auto custom-scrollbar bg-white rounded-lg shadow-md'>
+                <div className='w-full p-10 flex  items-center gap-8 '>
+                    <span className='text-4xl font-bold'>About</span>
+                    <div className=' font-bold border-b-2 w-60 border-blue-300'></div>
+                </div>
+                <div className='w-[90%] flex flex-col gap-2'>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur excepturi esse, laudantium similique vitae at mollitia dolorum dignissimos assumenda molestiae iure quae veritatis? Consequuntur perspiciatis iste consequatur temporibus quidem ipsum.
+
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur excepturi esse, laudantium similique vitae at mollitia dolorum dignissimos assumenda molestiae iure quae veritatis? Consequuntur perspiciatis iste consequatur temporibus quidem ipsum.
+
+                    </p>
+                </div>
+                <div className='text-2xl px-10 pt-4 font-semibold '>What I Do !</div>
+                <div className='w-full h-screen flex md:flex-wrap fl    ex-col '>
+                    {
+                        aboutSection.map((item, index) => (
+
+                            <div key={index} className="flex  relative w-full md:w-[45%] h-80 items-center justify-center flex-col m-2
+                group md:hover:bg-purple-600 rounded-lg py-2">
+                                <img src={item.image} alt='item' className="w-[300px] rounded-lg h-[230px] object-cover" />
+                                <h1 className='font-bold md:group-hover:text-white'>{item.title}</h1>
+                                <h1 className='text-[12px] md:group-hover:text-gray-200 text-gray-500 px-7'>{item.desc}</h1>
+                            </div>
+
+
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
